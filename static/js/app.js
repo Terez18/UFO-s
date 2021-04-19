d59915ae6b194b84e59c3fdb0357fdd4a1637804
Terez18 <tamarperez@comcast.net>
2021-04-18 20:01:28 -0700

    uploaded files for challenge including images

--- static/js/app.js
+++ static/js/app.js
@@ -22,7 +22,7 @@ function buildTable(data) {
       }
     );
   });
-
+}
   function handleClick() {
     // Grab the datetime value from the filter
     let date = d3.select("#datetime").property("value");
